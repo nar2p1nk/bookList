6d68680cc4c8a681dd374f8273d48c16fd5fabbb
Jack <farrisisqandar@gmail.com>
2021-07-31 02:54:28 +0800

    being careful

--- src/data.jsx
+++ src/data.jsx
@@ -20,8 +20,12 @@ const books =[
     {
         title: 'The Body Keeps the Score: Brain, Mind, and Body in the Healing of Trauma',
         author: 'Bessel van der Kolk M.D.',
-        picture:'https://images-na.ssl-images-amazon.com/images/I/41D3enj6JVS._SX324_BO1,204,203,200_.jpg',
-        description: `ass`
+        picture:`https://images-na.ssl-images-amazon.com/images/I/41D3enj6JVS._SY291_BO1,204,203,200_QL40_FMwebp_.jpg`,
+        description: `“Essential reading for anyone interested in understanding and treating traumatic stress and the scope of its impact on society.” —Alexander McFarlane, Director of the Centre for Traumatic Stress Studies \n
+
+        A pioneering researcher transforms our understanding of trauma and offers a bold new paradigm for healing in this New York Times bestseller
+         
+        Trauma is a fact of life. Veterans and their families deal with the painful aftermath of combat; one in five Americans has been molested; one in four grew up with alcoholics; one in three couples have engaged in physical violence. Dr. Bessel van der Kolk, one of the world’s foremost experts on trauma, has spent over three decades working with survivors. In The Body Keeps the Score, he uses recent scientific advances to show how trauma literally reshapes both body and brain, compromising sufferers’ capacities for pleasure, engagement, self-control, and trust. He explores innovative treatments—from neurofeedback and meditation to sports, drama, and yoga—that offer new paths to recovery by activating the brain’s natural neuroplasticity. Based on Dr. van der Kolk’s own research and that of other leading specialists, The Body Keeps the Score exposes the tremendous power of our relationships both to hurt and to heal—and offers new hope for reclaiming lives.`
     }
     
         
