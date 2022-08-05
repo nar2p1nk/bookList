7128e0750c50fd8cbdc8ff66cb66ba310c9fa12e
nar2p1nk <farrisisqandar@gmail.com>
2022-08-06 00:45:27 +0800

    these changes were made a long time ago

--- src/data.jsx
+++ src/data.jsx
@@ -26,6 +26,28 @@ const books =[
         A pioneering researcher transforms our understanding of trauma and offers a bold new paradigm for healing in this New York Times bestseller
          
         Trauma is a fact of life. Veterans and their families deal with the painful aftermath of combat; one in five Americans has been molested; one in four grew up with alcoholics; one in three couples have engaged in physical violence. Dr. Bessel van der Kolk, one of the world’s foremost experts on trauma, has spent over three decades working with survivors. In The Body Keeps the Score, he uses recent scientific advances to show how trauma literally reshapes both body and brain, compromising sufferers’ capacities for pleasure, engagement, self-control, and trust. He explores innovative treatments—from neurofeedback and meditation to sports, drama, and yoga—that offer new paths to recovery by activating the brain’s natural neuroplasticity. Based on Dr. van der Kolk’s own research and that of other leading specialists, The Body Keeps the Score exposes the tremendous power of our relationships both to hurt and to heal—and offers new hope for reclaiming lives.`
+    },{
+        title: 'The Truth About COVID-19: Exposing The Great Reset, Lockdowns, Vaccine Passports, and the New Normal ',
+        author: 'Doctor Joseph Mercola ',
+        picture: 'https://images-na.ssl-images-amazon.com/images/I/41EAWV6iNpL._SX331_BO1,204,203,200_.jpg',
+        description: `A Wall Street Journal and USA Today national bestseller!
+
+        Multiple New York Times best-selling author Dr. Joseph Mercola and Ronnie Cummins, founder and director of the Organic Consumers Association, team up to expose the truth―and end the madness―about COVID-19.
+        
+        Since early 2020, the world has experienced a series of catastrophic events―a global pandemic caused by what appears to be an engineered coronavirus; international lockdowns and border closings causing widespread business closures, economic collapse, and massive unemployment; and an unprecedented curtailment of civil liberties and freedoms in the name of keeping people safe by locking them up in their homes.
+        
+        We are now living in a world that is increasingly ruled, not by our democratic systems and institutions, but by public health fiat, carried out by politicians who rule by instilling fear and panic.
+        
+        In The Truth About COVID-19, Dr. Mercola and Cummins reveal new and emerging evidence that:
+        
+            The SARS-CoV-2 virus was, indeed, lab-engineered and emerged from a negligently managed bioweapons lab in Wuhan, China
+            The global pandemic was long anticipated by global elites who have used it to facilitate and hide the largest upward transfer of wealth in human history
+            PCR testing, case counts, morbidity, and vaccine safety and efficacy data have been widely manipulated and misrepresented
+            Obesity, diabetes, and heart disease are known to worsen COVID-19 outcomes, but the junk food industry continues to push its agenda at the expense of public health
+            Safe, simple, and inexpensive treatment and prevention for COVID-19 have been censored and suppressed to create a clear path for vaccine acceptance
+            Effectiveness of the vaccines has been wildly exaggerated and major safety questions have gone unanswered
+        
+        The good news in all of this is that we can take control of our health and that, together, we have the power to unite and fight back for our health, democracy, and freedom. The time is now for a global awakening. As Dr. Mercola and Cummins remind us, this is the fight of our lives.`
     }
     
         
